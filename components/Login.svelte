fb83723af1c9e20da87ae9df79fbaccd3047c733
awynnejo <89365137+awynnejo@users.noreply.github.com>
2022-08-24 16:43:30 +1000

    login form and spa template

---
+++ components/Login.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { link } from 'svelte-spa-router'
+    import { useForm, validators,  Hint, required } from "svelte-use-form"
+ const form = useForm();
+
+</script>
+<form use:form>
+    <h1>Login</h1>
+    <label>Username</label>
+    <input name='username' use:validators={[required]}/>
+    <Hint for='username' on='required'>
+        This field is required
+    </Hint>
+    <label>Password</label>
+    <input name='password' type='password' use:validators={[required]}>
+    <Hint for='password' on='required'>
+        This field is required
+    </Hint>
+    <button disabled={!$form.valid}>Login</button>
+</form>
---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let name: string;
+ import Router, {location, link} from 'svelte-spa-router';
+ import Home from '../components/Home.svelte'
+ import Login from '../components/Login.svelte'
+ import Game from '../components/Game.svelte'
+ import Games from '../components/Games.svelte'
+ import GameLog from '../components/GameLog.svelte'
+</script>
+
+<nav>
+	<a href="/">Gomoku</a>
+	<a href="/#/Login">Login</a>
+	<a href="/#/Game">Game</a>
+	<a href="/#/Games">Games</a>
+	<a href="/#/GameLog">GameLog</a>
+
+</nav>
+
+<Router routes={{
+			   '/':Home,
+			   '/Login':Login,
+			   '/Game':Game,
+			   '/Games':Games,
+			   '/GameLog':GameLog
+			   }}/>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
