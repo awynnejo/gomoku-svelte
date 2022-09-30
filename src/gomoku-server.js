e1fd95211ed6fff25ec855828fe3fe903b22486c
awynnejo <89365137+awynnejo@users.noreply.github.com>
2022-09-30 18:17:10 +1000

    endpoints

--- src/gomoku-server.js
+++ src/gomoku-server.js
@@ -11,3 +11,28 @@ const path = require('path');
 app.listen(port, () => {
    console.log(`Server is up at port ${port}`);
 });
+
+app.use(express.json())
+
+app.use('api/v2/createuser', (req, res, next) =>{
+   console.log("createuser")
+})
+
+app.use('api/v2/login', (req, res, next) =>{
+   console.log('login')
+})
+
+app.use('api/v2/games', (req, res, next) =>{
+   console.log("games")
+})
+
+app.use('api/v2/gamedetails', (req, res, next) =>{
+   console.log('gamedetails')
+})
+
+app.use('api/v2/newgame', (req, res, next) =>{
+   console.log("newgame")
+
+})
+
+app.use('api/v2/')
